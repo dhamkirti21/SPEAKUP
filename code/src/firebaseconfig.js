faf19b316a206e5289cf422e9f3ef296a54bec28
Dhamkirti Sisodia <dksisodia002@gmail.com>
2023-10-16 16:28:11 +0530

    Update firebaseconfig.js

--- code/src/firebaseconfig.js
+++ code/src/firebaseconfig.js
@@ -8,7 +8,12 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  {"Enter Your Config!!"}
+ apiKey: "AIzaSyCLWpcKbt_ipmwRTIvpfbX17BWP8UGI8Co",
+  authDomain: "speak-up-ae15f.firebaseapp.com",
+  projectId: "speak-up-ae15f",
+  storageBucket: "speak-up-ae15f.appspot.com",
+  messagingSenderId: "24332814255",
+  appId: "1:24332814255:web:6369cb2612708d09b5115a"
 };
 
 // Initialize Firebase
